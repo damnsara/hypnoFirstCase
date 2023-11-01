2fe1dc9702f659d31b494d79eb2d944762f34be2
richbertoliveira <richbert.faria@sga.pucminas.br>
2023-10-31 21:22:32 -0300

    first commit

---
+++ FootballTeam.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechnicalCase
+{
+    public class FootballTeam
+    {
+        public List<int> matches;
+
+        public FootballTeam() { 
+            matches = new List<int>();
+        }
+
+        public void addScore(int score)
+        {
+            matches.Add(score);
+        }
+
+        public void emptyListOfMatches()
+        {
+            matches.Clear();
+        }
+
+    }
+}
